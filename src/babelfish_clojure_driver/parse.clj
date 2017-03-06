16200da69b938d8a4fc4e9328058921c99e2433f
Miguel Molina <hi@mvader.me>
2017-03-06 15:21:17 +0100

    native: expand defn and treat some other forms as invoke

--- src/babelfish_clojure_driver/parse.clj
+++ src/babelfish_clojure_driver/parse.clj
@@ -1,22 +1,87 @@
 (ns babelfish-clojure-driver.parse
-  (:require [clojure.tools.analyzer :refer [analyze-in-env]]
+  (:require [clojure.tools.analyzer :as ana]
             [clojure.string :refer [trim]]
             [clojure.tools.analyzer.utils :refer [ctx]])
   (:import java.io.IOException
            java.io.StringReader
            java.io.BufferedReader))
 
+(defn- parse-form-with-exprs
+  ([form env op]
+   (parse-form-with-exprs form env op {}))
+
+  ([[_ & exprs :as form] env op opts]
+   (let [exprs (mapv (ana/analyze-in-env (ctx env :ctx/expr)) exprs)]
+     (merge {:op       op
+             :env      env
+             :form     form
+             :exprs    exprs
+             :meta     (meta form)
+             :children (concat [:exprs] (keys opts))}
+            opts))))
+
+(defn parse-defn
+  [[_ sym & exprs :as form] env]
+  (when (not (symbol? sym))
+    (throw (str "First argument to defn must be a symbol, had: " (class sym))))
+
+  (when (and (namespace sym))
+    (throw (str "Cannot defn namespaced symbol: " sym)))
+
+  (let [docs?      (string? (first exprs))
+        m          (if docs?
+                     {:doc (first exprs)}
+                     {})
+        exprs      (if docs?
+                     (rest exprs)
+                     exprs)
+        attrs?     (map? (first exprs))
+        m          (if attrs?
+                     (conj m (first exprs))
+                     m)
+        exprs      (if attrs?
+                     (rest exprs)
+                     exprs)
+        attrs-end? (map? (last exprs))
+        m          (if attrs-end?
+                     (conj m (last exprs))
+                     m)
+        exprs      (if attrs-end?
+                     (butlast exprs)
+                     exprs)
+        fdecl      (list :def
+                         (with-meta sym (merge m (meta form)))
+                         (cons 'fn exprs))
+        parsed     (ana/parse-def fdecl env)]
+    (if (:doc m)
+      (assoc parsed :doc (:doc m))
+      parsed)))
+
+(defn parse-fn
+  "Parses the fn form correctly. Using parse-fn* directly results in a
+  node of type with-meta, instead of fn"
+  [[_ & exprs] env]
+  (ana/parse-fn* (concat [:fn] exprs) env))
+
+(defn parse-letfn
+  "Parses the letfn macro."
+  [[_ & exprs] env]
+  (let [[fnspecs & body] exprs
+        form (concat (concat [:letfn*] [(vec (interleave (map first fnspecs)
+                                                         (map #(cons 'fn %) fnspecs)))])
+                     body)]
+    (ana/parse-letfn* form env)))
+
 (defn parse-recur
   "Parse recur is the same parse function as the one in clojure.tools.analyzer
   but it does not validate it."
-  [[_ & exprs :as form] env]
-  (let [exprs (mapv (analyze-in-env (ctx env :ctx/expr)) exprs)]
-    {:op          :recur
-     :env         env
-     :form        form
-     :exprs       exprs
-     :meta        (meta form)
-     :children    [:exprs]}))
+  [form env]
+  (parse-form-with-exprs form env :recur))
+
+(defn parse-comments
+  "Parses the comment form as a special form instead of an invoke one."
+  [form env]
+  (parse-form-with-exprs form env :comment))
 
 (defn comment-node
   "Creates a new comment node with the given text, line and columns."
@@ -30,17 +95,6 @@
               :end-line line
               :end-col end-col}
    :children [:exprs]})
-
-(defn parse-comments
-  "Parses the comment form as a special form instead of an invoke one."
-  [[_ & exprs :as form] env]
-  (let [exprs (mapv (analyze-in-env (ctx env :ctx/expr)) exprs)]
-    {:op       :comment
-     :env      env
-     :form     form
-     :exprs    exprs
-     :meta     (meta form)
-     :children [:exprs]}))
 
 (defn extract-comments
   "Extracts the comments with `;` from the source code. `comment` form is not
@@ -56,7 +110,7 @@
            start-pos   0
            pos         0
            nodes       []]
-      (let [ch (try 
+      (let [ch (try
                  (let [c (.read r)]
                    (if (>= c 0) (char c) :end))
                  (catch IOException e :end))
